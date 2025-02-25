33d774f2390d575cd6e27004424b689d94c34a43
saketvivek777 <saketvivek777@gmail.com>
2025-02-25 16:55:59 +0530

    add: Uploaded

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,16 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//     optimizeDeps: {
-//     exclude: ['lucide-react'],
-//     base:"/Blockchain_Based_Certification_React_app/",
-//     build:{
-//       chunkSizeWarningLimit:2000,
-//     }
-//     }
-// })
-
-
+ 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Blockchain_Certificate_Generation/",
-  build: {
-    chunkSizeWarningLimit: 2000, // Increase the chunk size limit (Default: 500KB)
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("html2canvas")) {
-              return "html2canvas"; // Separate html2canvas into its own chunk
-            }
-            return "vendor"; // Separate vendor libraries
-          }
-        },
-      },
-    },
-  },
+  base: "/Blockchain_Based_Certification_React_app/",
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    chunkSizeWarningLimit: 2000, // Increase the limit (default is 500 KB)
   }
 })
+
